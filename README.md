43e9e29581c521de724a2288c9338fc390ca5560
debb <debora.leticia11.dl@gmail.com>
2026-05-21 16:15:03 -0300

    add monitoramento prometheus e grafana

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # sd-ecommerce-seguro
+
+monitoramento Passo a passo
+
+1 - abre o Docker Desktop
+
+2 - rodar backend
+cd backend
+npm run dev
+
+3 - abrir monitoramento (OUTRO TERMINAL)
+cd monitoring
+docker compose up
+
+4 - abrir navegador
+rota de métricas: http://localhost:3000/metrics
+
+5 - abrir grafana
+http://localhost:3001
+user e senha: admin
+
+6 - abrir dashboard
+
+7 - faz requisições no postman e observa os gráficos 
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const helmet = require("helmet");
 require("dotenv").config();
 
+const promBundle = require("express-prom-bundle");
+const client = require("prom-client");
+
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const limiter = require("./middleware/rateLimitMiddleware");
@@ -11,9 +14,21 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const app = express();
 
 
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  includeStatusCode: true,
+  promClient: {
+    collectDefaultMetrics: {}
+  }
+});
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
+
+app.use(metricsMiddleware);
 
 app.use(limiter);
 
@@ -26,6 +41,11 @@ app.get("/", (req, res) => {
   res.json({
     message: "API de segurança do e-commerce funcionando"
   });
+});
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 const PORT = 3000;
